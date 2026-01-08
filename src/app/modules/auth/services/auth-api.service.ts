3c3f3952b2cd78e78b7be63a7b013f75f5249b50
Your Name <your@example.com>
2026-01-08 01:17:10 -0500

    login + sidebar

---
+++ src/app/modules/auth/services/auth-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoginRequest } from '../interfaces/login.interface';
+import { AuthResponse } from '../../../shared/interfaces/usuario.interface';
+import { environment } from '../../../../environments/environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthApiService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  login(data: LoginRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/login`, data);
+  }
+}
